d238f8a4e6af18159af8b19e7aa6527e3afa9047
Guilherme Soares Oliveira <guilherme.soares@dcomp.ufs.br>
2026-07-13 22:12:10 -0300

    corrige erros de salvar/abrir e ajusta o desenho do triangulo

--- src/paint_mvc/modelo/figuras.py
+++ src/paint_mvc/modelo/figuras.py
@@ -8,36 +8,55 @@ class Figura:
         self.cor_preenchimento = cor_preenchimento
 
     def to_dict(self):
-        return{
-            "tipo": self._class_._name_,
-            "x1": self.x2,"y1": self.y1,
-            "x2": self.x2,"y2": self.y2,
+        return {
+            "tipo": self.__class__.__name__,
+            "x1": self.x1, "y1": self.y1,
+            "x2": self.x2, "y2": self.y2,
             "cor_borda": self.cor_borda,
             "cor_preenchimento": self.cor_preenchimento
         }
 
     @classmethod
-    def from_dict(cls,data):
-        return cls(data["x1], data["y1"], data["x2"], data["y2"], data["cor_borda"], data["cor_preenchimento"])
+    def from_dict(cls, data):
+        return cls(
+            x1=data["x1"],
+            y1=data["y1"],
+            x2=data["x2"],
+            y2=data["y2"],
+            cor_borda=data["cor_borda"],
+            cor_preenchimento=data["cor_preenchimento"]
+        )
 
 class Linha(Figura):
     def desenhar(self, canvas):
-        canvas.create_line(self.x1, self.y1, self.x2, self.y2,fill=self.cor_borda)
+        canvas.create_line(self.x1, self.y1, self.x2, self.y2, fill=self.cor_borda)
 
 class Retangulo(Figura):
     def desenhar(self, canvas):
-        canvas.create_rectangle(self.x1, self.y1, self.x2, self.y2,outline=self.cor_borda,fill=self.cor_preenchimento)
+        canvas.create_rectangle(self.x1, self.y1, self.x2, self.y2, outline=self.cor_borda, fill=self.cor_preenchimento)
 
 class Oval(Figura):
     def desenhar(self, canvas):
-        canvas.create_oval(self.x1, self.y1, self.x2, self.y2,outline=self.cor_borda,fill=self.cor_preenchimento)
-
+        canvas.create_oval(self.x1, self.y1, self.x2, self.y2, outline=self.cor_borda, fill=self.cor_preenchimento)
 
 class Poligono(Figura):
     def desenhar(self, canvas):
-        x3 = (self.x1 + self.x2) // 2
-        y3 = self.y1
-        canvas.create_polygon(self.x1, self.y2,self.x2, self.y2,x3, y3,outline=self.cor_borda,fill=self.cor_preenchimento)
+        topo_x = (self.x1 + self.x2) // 2
+        topo_y = self.y1
+        
+        base_esq_x = self.x1
+        base_esq_y = self.y2
+        
+        base_dir_x = self.x2
+        base_dir_y = self.y2
+        
+        canvas.create_polygon(
+            topo_x, topo_y, 
+            base_esq_x, base_esq_y, 
+            base_dir_x, base_dir_y, 
+            outline=self.cor_borda, 
+            fill=self.cor_preenchimento
+        )
 
 class MaoLivre(Figura):
     def __init__(self, cor_borda):
@@ -56,6 +75,6 @@ class MaoLivre(Figura):
 
     @classmethod
     def from_dict(cls, data):
-        obj = cls(data["cor_borda"])
+        obj = cls(cor_borda=data["cor_borda"])
         obj.pontos = [tuple(p) for p in data["pontos"]]
         return obj
